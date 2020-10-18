bd672d7304191a8dff65a3024b06ee1e6cbbff4d
mike <mikesapienzaaudio@gmail.com>
2020-10-18 08:16:53 -0500

    eh confused

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,22 @@
+class SessionsController < ApplicationController
+
+    def index
+
+    end
+
+    def new
+
+    end
+
+    def create        
+        return redirect_to(controller: 'sessions',action: 'new') if !params[:name] || params[:name].empty?
+        session[:name] = params[:name]
+        redirect_to controller: 'application', action: 'hello'
+    end
+
+    def destroy
+        session.delete :name
+        redirect_to :root
+    end
+    
+end
